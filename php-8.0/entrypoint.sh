2fb0f6757a1f27729fee4f5c9ae94660ffb7faee
Damian Nowak <me@dnowak.dev>
2021-01-04 12:52:07 +0100

    update docker versions

--- php-8.0/entrypoint.sh
+++ php-8.0/entrypoint.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
 # Load BlackFire
-if [ "${ENABLE_BLACKFIRE}" != "1" ]; then
-	rm -f /usr/local/etc/php/conf.d/blackfire.ini
-fi
+# if [ "${ENABLE_BLACKFIRE}" != "1" ]; then
+# 	rm -f /usr/local/etc/php/conf.d/blackfire.ini
+# fi
 
 # Load xDebug
-if [ "${ENABLE_XDEBUG}" != "1" ]; then
-	rm -f /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
-fi
+# if [ "${ENABLE_XDEBUG}" != "1" ]; then
+# 	rm -f /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
+# fi
 
-service php-fpm restart
+# service php-fpm restart
+exec /usr/local/bin/docker-entrypoint.sh "$@"
